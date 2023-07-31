f282828b6d63fedf7620574ef601ee8cf887cc49
Luancss <ventobo123@gmail.com>
2023-07-31 17:52:09 -0300

    GET e POST

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,30 +3,41 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
+
+
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(event) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if(token) {
+      getUser(token);
+    }
+  }, [])
+
+  async function getUser(token) {
+    const {url, options} = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+   async function handleSubmit(event) {
     event.preventDefault();
 
     if(username.validate() && password.validate()) {
-
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json)
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
       });
+  
+     const response = await fetch(url, options)
+     const json = await response.json();
+     window.localStorage.setItem('item', json.token)
+     getUser(json.token);
   }
 }
 
---
+++ src/api.js
@@ -0,0 +1,28 @@
+export const API_URL = 'https://dogsapi.origamid.dev/json'
+
+export function TOKEN_POST(body) {
+  return {
+    url: API_URL + '/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'appication/json',
+      },
+      body: JSON.stringify(body),
+    }
+  }
+}
+
+export function USER_GET(token) {
+  return {
+    url: API_URL + '/api/user',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer' + token,
+      },
+    }
+  }
+}
+    
+  
