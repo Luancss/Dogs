c2e72ab73e9710a701b9c5bd71e3bf220ce6a90c
Luancss <ventobo123@gmail.com>
2023-07-31 16:01:06 -0300

    Form com regex completo

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    if(username.validate() && password.validate()) {
+
     fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify(),
     })
       .then((response) => {
         console.log(response);
@@ -24,14 +28,15 @@ const LoginForm = () => {
         console.log(json)
       });
   }
+}
 
   return (
     <section>
       Login Form
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label='Usuário' type='text' name='username' />
-        <Input label='Senha' type='password' name='password' />
+        <Input label='Usuário' type='text' name='username' {...username}/>
+        <Input label='Senha' type='password' name='password' {...password}/>
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
