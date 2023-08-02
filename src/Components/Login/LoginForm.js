fe3534cac546135db3916846ea14272509fae7de
Luancss <ventobo123@gmail.com>
2023-08-01 23:27:42 -0300

    userContext

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-import { TOKEN_POST, USER_GET } from "../../api";
+import { UserContext } from "../../UserContext";
 
 
 
@@ -11,33 +11,14 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if(token) {
-      getUser(token);
-    }
-  }, [])
+  const {userLogin} = React.useContext(UserContext);
 
-  async function getUser(token) {
-    const {url, options} = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
 
    async function handleSubmit(event) {
     event.preventDefault();
 
     if(username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-  
-     const response = await fetch(url, options)
-     const json = await response.json();
-     window.localStorage.setItem('item', json.token)
-     getUser(json.token);
+      userLogin(username.value, password.value)
   }
 }
 
