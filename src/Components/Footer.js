843fc784e0640e2e8480ddb53e5384f8bfe0abd5
Luancss <ventobo123@gmail.com>
2023-08-02 17:44:06 -0300

    Botões de login criados

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>Footer</div>
+    <div className={styles.footer}></div>
   )
 }
 
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import { UserContext } from "../../UserContext";
+import Error from "../Helper/Error";
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Forms/Button.module.css'
 
 
 
@@ -11,32 +14,35 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const {userLogin, error, loading} = React.useContext(UserContext);
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
-
-   async function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    if(username.validate() && password.validate()) {
+    if (username.validate() && password.validate()) {
       userLogin(username.value, password.value)
+    }
   }
-}
 
   return (
-    <section>
-      Login Form
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <Input label='Usuário' type='text' name='username' {...username}/>
-        <Input label='Senha' type='password' name='password' {...password}/>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label='Usuário' type='text' name='username' {...username} />
+        <Input label='Senha' type='password' name='password' {...password} />
         {loading ? (
-        <Button disabled>Carregando...</Button>
+          <Button disabled>Carregando...</Button>
         ) : (
-        <Button>Entrar</Button>
+          <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+      </div>
     </section>
   );
 };
