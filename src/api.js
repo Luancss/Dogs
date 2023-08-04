ccb0a207fe28824d877d4f77233d7a94e28b1f2b
Luancss <ventobo123@gmail.com>
2023-08-04 17:38:57 -0300

    Feed para postagem de fotos

--- src/api.js
+++ src/api.js
@@ -50,3 +50,16 @@ export const TOKEN_VALIDATE_POST = (token) => {
   };
 };
 
+export function PHOTO_POST (formData, token) {
+  return {
+    url: API_URL + '/api/photo',
+    options: {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    },
+  };
+};
+
