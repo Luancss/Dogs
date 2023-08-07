1cca1a9bca429eb38f95c0bd739c6a7c5a5e608a
Luancss <ventobo123@gmail.com>
2023-08-07 17:34:09 -0300

    Botão para deletar foto

---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  const handleClick = async (event) => {
+    const confirm = window.confirm('Tem certeza que deseja deletar?');
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) {
+        window.location.reload();
+      }
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletando...
+        </button>
+      ) : (
+        <button className={styles.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
--- src/api.js
+++ src/api.js
@@ -97,6 +97,18 @@ export const COMMENT_POST = (id, body) => {
   };
 };
 
+export const PHOTO_DELETE = (id) => {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer' + window.localStorage.getItem('token') ,
+      },
+    },
+  };
+};
+
 
 
 
