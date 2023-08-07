209926de0fe6411255c948e0f1a26d3f4d3307a9
Luancss <ventobo123@gmail.com>
2023-08-07 17:09:00 -0300

    Adicionar comentários ao post

--- src/api.js
+++ src/api.js
@@ -83,6 +83,20 @@ export const PHOTOS_GET = (id) => {
   };
 };
 
+export const COMMENT_POST = (id, body) => {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer' + window.localStorage.getItem('token') ,
+      },
+      body: JSON.stringify(body)
+    },
+  };
+};
+
 
 
 
