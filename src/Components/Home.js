8dc7f06a9560b39ebbd6845b243ddb5cf539eaf8
Luancss <ventobo123@gmail.com>
2023-08-08 17:17:38 -0300

    Adicionando efeito de loading

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Feed from './Feed/Feed'
+import Loading from './Helper/Loading'
 
 const Home = () => {
   return (
     <section className='container mainContainer'>
+      
       <Feed/>
     </section>
   )
